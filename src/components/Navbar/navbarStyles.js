1b76297e9c50d2989698ce06af8a4e8af0105ac0
Sathis <rsathisk15@gmail.com>
2023-01-10 19:04:41 +0100

    Navbar and SocialIcon component update

--- src/components/Navbar/navbarStyles.js
+++ src/components/Navbar/navbarStyles.js
@@ -1,45 +1,45 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   grow: {
     flexGrow: 1,
   },
   navBar: {
-    // backgroundColor: "#1877F2",
+    backgroundColor: '#0a66c2',
     // background: "rgb(255,102,179)",
-    background:
-      "linear-gradient(90deg, rgba(255,102,179,1) 0%, rgba(194,148,233,1) 100%)",
-    boxShadow: "none",
-    color: "#1B1B1E",
+    // background:
+    //   "linear-gradient(90deg, rgba(255,102,179,1) 0%, rgba(194,148,233,1) 100%)",
+    boxShadow: 'none',
+    color: '#1B1B1E',
   },
   title: {
-    display: "block",
+    display: 'block',
     margin: theme.spacing(1),
-    marginLeft: "2%",
+    marginLeft: '2%',
     paddingRight: theme.spacing(1),
     paddingLeft: theme.spacing(1),
-    fontSize: "30px",
-    color: "#FFFFFF",
-    fontFamily: "Yellowtail, cursive",
-    "&:hover": {
-      cursor: "pointer",
+    fontSize: '30px',
+    color: '#FFFFFF',
+    fontFamily: 'Yellowtail, cursive',
+    '&:hover': {
+      cursor: 'pointer',
     },
-    [theme.breakpoints.down("sm")]: {
-      fontSize: "25px",
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '25px',
     },
   },
   navButtons: {
-    background: "transparent",
-    boxShadow: "none",
-    color: "#FFF",
-    border: "1px solid #FFF",
-    textTransform: "none",
+    background: 'transparent',
+    boxShadow: 'none',
+    color: '#FFF',
+    border: '1px solid #FFF',
+    textTransform: 'none',
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
-    paddingRight: "30px",
-    paddingLeft: "30px",
-    "&:hover": {
-      background: "transparent",
+    paddingRight: '30px',
+    paddingLeft: '30px',
+    '&:hover': {
+      background: 'transparent',
       // boxShadow: "none",
     },
   },
@@ -47,25 +47,25 @@ const useStyles = makeStyles((theme) => ({
     marginLeft: theme.spacing(1),
     marginRight: theme.spacing(1),
     padding: theme.spacing(1),
-    color: "#FFFFFF",
-    border: "2px solid transparent",
-    "&:hover": {
-      cursor: "pointer",
-      color: "#FFFFFF",
-      borderBottom: "2px solid #1877F2",
+    color: '#FFFFFF',
+    border: '2px solid transparent',
+    '&:hover': {
+      cursor: 'pointer',
+      color: '#FFFFFF',
+      borderBottom: '2px solid #1877F2',
     },
   },
 
   sectionDesktop: {
-    display: "none",
-    [theme.breakpoints.up("md")]: {
-      display: "flex",
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
     },
   },
   sectionMobile: {
-    display: "flex",
-    [theme.breakpoints.up("md")]: {
-      display: "none",
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
     },
   },
 }));
--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -1,25 +1,25 @@
-import React from "react";
-import IconButton from "@material-ui/core/IconButton";
-import FacebookIcon from "@material-ui/icons/Facebook";
-import InstagramIcon from "@material-ui/icons/Instagram";
-import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import React from 'react';
+import IconButton from '@material-ui/core/IconButton';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
 import GitHubIcon from '@material-ui/icons/GitHub';
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   socialIcons: {
-    fontSize: "23px",
-    display: "flex",
-    alignItem: "center",
-    textAlign: "center",
-    padding: "8px",
-    margin: "10px",
-    color: "white",
-    cursor: "pointer",
-    transition: "transform .4s",
-    "&:hover": {
-      color: "#FFF",
-      transform: "scale(1.1)",
+    fontSize: '23px',
+    display: 'flex',
+    alignItem: 'center',
+    textAlign: 'center',
+    padding: '8px',
+    margin: '10px',
+    color: 'white',
+    cursor: 'pointer',
+    transition: 'transform .4s',
+    '&:hover': {
+      color: '#FFF',
+      transform: 'scale(1.1)',
     },
   },
 }));
@@ -27,29 +27,29 @@ const SocialIcons = () => {
   const classes = useStyles();
   const socialIcons = [
     {
-      bgColor: "#0A66C2",
-      link: "https://www.linkedin.com/in/rsathisk15/",
+      bgColor: '#0A66C2',
+      link: 'https://www.linkedin.com/in/rsathisk15/',
       icon: <LinkedInIcon />,
-      label: "LinkedIn",
+      label: 'LinkedIn',
+    },
+    {
+      bgColor: '#171515',
+      link: 'https://github.com/sathisk15',
+      icon: <GitHubIcon />,
+      label: 'GitHub',
     },
     {
       bgColor:
-        "radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%)",
-      link: "https://www.instagram.com/sathisk15/",
+        'radial-gradient(circle at 30% 107%, #fdf497 0%, #fdf497 5%, #fd5949 45%,#d6249f 60%,#285AEB 90%)',
+      link: 'https://www.instagram.com/sathisk15/',
       icon: <InstagramIcon />,
-      label: "Instagram",
+      label: 'Instagram',
     },
     {
-      bgColor: "#1877F2",
-      link: "https://www.facebook.com/sathiskumar.sathiskumar.1048554",
+      bgColor: '#1877F2',
+      link: 'https://www.facebook.com/sathiskumar.sathiskumar.1048554',
       icon: <FacebookIcon />,
-      label: "Facebook",
-    },
-    {
-      bgColor: "#171515",
-      link: "https://github.com/sathisk15",
-      icon: <GitHubIcon />,
-      label: "GitHub",
+      label: 'Facebook',
     },
   ].map((icon, i) => (
     <IconButton
@@ -64,8 +64,8 @@ const SocialIcons = () => {
     </IconButton>
   ));
   return (
-    <div style={{ display: "flex" }}>
-      <div style={{ margin: "auto", display: "flex" }}>{socialIcons}</div>
+    <div style={{ display: 'flex' }}>
+      <div style={{ margin: 'auto', display: 'flex' }}>{socialIcons}</div>
     </div>
   );
 };

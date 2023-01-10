0dbd7482e1a7c70ad696c435a020cf5abed8a0dd
Sathis <rsathisk15@gmail.com>
2023-01-10 19:29:40 +0100

    Skills updated

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,50 +1,62 @@
-import { Box, Container, Grid, Typography, Paper } from "@material-ui/core";
-import React, { Fragment } from "react";
-import useStyles from "./skillStyles.js";
+import { Box, Container, Grid, Typography, Paper } from '@material-ui/core';
+import React, { Fragment } from 'react';
+import useStyles from './skillStyles.js';
 // Skills png
-import html from "./images/html.png";
-import css from "./images/css.png";
-import js from "./images/js.png";
-import react from "./images/react.png";
-import bootstrap from "./images/bootstrap.png";
-import mongodb from "./images/mongodb.png";
-import nodejs from "./images/nodejs.png";
-import redux from "./images/redux.png";
-import express from "./images/express.png";
-import network from "./images/networking.png";
-import mui from "./images/materialUi.png";
+import html from './images/html.png';
+import css from './images/css.png';
+import js from './images/js.png';
+import react from './images/react.png';
+import bootstrap from './images/bootstrap.png';
+import mongodb from './images/mongodb.png';
+import nodejs from './images/nodejs.png';
+import redux from './images/redux.png';
+import express from './images/express.png';
+// import network from './images/networking.png';
+import mui from './images/materialUi.png';
+import git from './images/git.png';
+import github from './images/github.png';
 // Skills Data
 const Skills = () => {
   const classes = useStyles();
   const skillsArray = [
-    { img: html, name: "HTML", link: "https://www.w3schools.com/html/" },
-    { img: css, name: "CSS", link: "https://www.w3schools.com/css/" },
-    {
-      img: js,
-      name: "Javascript",
-      link: "https://www.w3schools.com/js/default.asp",
-    },
     {
       img: react,
-      name: "React",
-      link: "https://reactjs.org/docs/getting-started.html",
+      name: 'React',
+      link: 'https://reactjs.org/docs/getting-started.html',
     },
     {
       img: redux,
-      name: "Redux",
-      link: "https://redux.js.org/introduction/getting-started",
+      name: 'Redux',
+      link: 'https://redux.js.org/introduction/getting-started',
     },
-    { img: nodejs, name: "Nodejs", link: "https://nodejs.org/en/docs/" },
     {
-      img: mongodb,
-      name: "MongoDB",
-      link: "https://docs.mongodb.com/manual/tutorial/",
+      img: js,
+      name: 'Javascript',
+      link: 'https://www.w3schools.com/js/default.asp',
     },
-    { img: mui, name: "M Ui", link: "https://material-ui.com/" },
+    { img: html, name: 'HTML', link: 'https://www.w3schools.com/html/' },
+    { img: css, name: 'CSS', link: 'https://www.w3schools.com/css/' },
+    {
+      img: git,
+      name: 'Git',
+      link: 'https://git-scm.com/',
+    },
+    {
+      img: github,
+      name: 'GitHub',
+      link: 'https://github.com/',
+    },
+    { img: mui, name: 'M Ui', link: 'https://material-ui.com/' },
     {
       img: bootstrap,
-      name: "Bootstrap",
-      link: "https://getbootstrap.com/docs/5.0/getting-started/introduction/",
+      name: 'Bootstrap',
+      link: 'https://getbootstrap.com/docs/5.0/getting-started/introduction/',
+    },
+    { img: nodejs, name: 'Nodejs', link: 'https://nodejs.org/en/docs/' },
+    {
+      img: mongodb,
+      name: 'MongoDB',
+      link: 'https://docs.mongodb.com/manual/tutorial/',
     },
   ].map((skill, i) => (
     <Grid item xs={4} sm={4} md={3} lg={2} key={i}>
@@ -82,39 +94,39 @@ const Skills = () => {
             >
               {skillsArray}
 
-              <Grid item xs={4} sm={4} md={3} lg={2} key={"express"}>
+              <Grid item xs={4} sm={4} md={3} lg={2} key={'express'}>
                 <Paper
                   elevation={3}
                   className={classes.skillImgContainer}
-                  onClick={() => window.open("https://expressjs.com/")}
+                  onClick={() => window.open('https://expressjs.com/')}
                 >
                   <img
                     src={express}
                     alt="express"
                     className={classes.skillImg}
-                    style={{ margin: "42%" }}
+                    style={{ margin: '42%' }}
                   />
                 </Paper>
               </Grid>
-              <Grid item xs={4} sm={4} md={3} lg={2} key={"MTA"}>
+              {/* <Grid item xs={4} sm={4} md={3} lg={2} key={'MTA'}>
                 <Paper
                   elevation={3}
                   className={classes.skillImgContainer}
                   onClick={() =>
                     window.open(
-                      "https://support.microsoft.com/en-us/topic/earn-a-microsoft-technology-associate-mta-certification-357215d0-31ce-0620-feba-1bb60165b770"
+                      'https://support.microsoft.com/en-us/topic/earn-a-microsoft-technology-associate-mta-certification-357215d0-31ce-0620-feba-1bb60165b770'
                     )
                   }
                 >
                   <img
                     src={network}
                     alt="networking"
-                    className={classes.skillImg}
-                    style={{ margin: "20%" }}
+                    className={classes.network}
+                    style={{ margin: '20%' }}
                   />
                   <Typography variant="body1">Network</Typography>
                 </Paper>
-              </Grid>
+              </Grid> */}
             </Grid>
           </Grid>
         </Container>
--- src/components/Skills/skillStyles.js
+++ src/components/Skills/skillStyles.js
@@ -1,37 +1,37 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 const useStyles = makeStyles((theme) => ({
   skillsSection: {
-    padding: "5px",
-    margin: "50px 0",
+    padding: '5px',
+    margin: '50px 0',
   },
   skillsHeading: {
-    padding: "5px",
-    margin: "5px auto",
-    borderBottom: "2px solid #D268CC",
+    padding: '5px',
+    margin: '5px auto',
+    borderBottom: '2px solid #0a66c2',
   },
   skillsContent: {
-    margin: "20px 0",
+    margin: '20px 0',
   },
   skillImgContainer: {
-    width: "100%",
-    padding: "5px",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    transition: "transform .4s",
+    width: '100%',
+    padding: '5px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    transition: 'transform .4s',
 
-    "&:hover": {
-      transform: "scale(1.1)",
-      cursor: "pointer",
-      position: "relative",
-      overflow: "hidden",
-      boxShadow: "6px 7px 40px -4px rgba(0, 0, 0, 0.2)",
+    '&:hover': {
+      transform: 'scale(1.1)',
+      cursor: 'pointer',
+      position: 'relative',
+      overflow: 'hidden',
+      boxShadow: '6px 7px 40px -4px rgba(0, 0, 0, 0.2)',
     },
   },
   skillImg: {
-    maxWidth: "100%",
-    maxHeight: "100%",
+    maxWidth: '100%',
+    maxHeight: '100%',
   },
 }));
 
